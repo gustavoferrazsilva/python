3aa722b32d99fd39f659a11b42b22260516f1979
gustavoferrazsilva <117416161+gustavoferrazsilva@users.noreply.github.com>
2022-11-03 22:22:52 -0300

    Add files via upload

---
+++ exer03.py
@@ -0,0 +1,17 @@
+#trabalhando com variaveis
+#num1 = 10 
+#num2 = 5
+num1= input('Informe o primeiro valor:')
+num2= input('Informe o segundo valor:')
+
+print (type (num1))
+
+soma = num1 + num2 
+print (' A soma é: ', soma)
+
+soma1 = int(num1) + int(num2)
+print ('A soma correta é: {}'. format(soma1))
+
+#{} reserva valor para entrar dentro da função
+# format está relacionado a substituição das chaves
+#input é para entrada de dados
