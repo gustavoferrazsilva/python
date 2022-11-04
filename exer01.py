ecfdb0d7f8618960f34f578b339d756744a902b4
gustavoferrazsilva <117416161+gustavoferrazsilva@users.noreply.github.com>
2022-11-03 22:03:23 -0300

    Add files via upload

---
+++ exer01.py
@@ -0,0 +1,4 @@
+#caracter de comentario
+#print para imprimir informacoes na tela
+print ('Olá Mundo python')
+print ('Meu nome é: ', 'Gustavo', 27, 'Anos')
---
+++ exer02.py
@@ -0,0 +1,5 @@
+#usando input para entrada de dados
+print ('Seja bem-vindo a aula de ' + 'Python')
+
+#chave para indicar a posição de substitução
+print ('Ola {}' . format(input('Qual o seu nome? ')))
