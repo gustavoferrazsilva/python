3144ca959d286200e73d6def845efd10cf94ed54
gustavoferrazsilva <117416161+gustavoferrazsilva@users.noreply.github.com>
2022-11-08 19:56:36 -0300

    Add files via upload

---
+++ exer04.py
@@ -0,0 +1,7 @@
+num1= int(input('Informe o primeiro valor:'))
+num2= int(input('Informe o segundo valor:'))
+
+mult= num1 * num2
+divisao= num1 / num2
+
+print(' A multiplicação é: {} - a divisão é {}'.format(mult,divisao))
