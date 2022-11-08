fb58751aecdb720c37ac46ca58a85d83a27921ff
gustavoferrazsilva <117416161+gustavoferrazsilva@users.noreply.github.com>
2022-11-08 20:27:38 -0300

    Add files via upload

---
+++ exer06.py
@@ -0,0 +1,10 @@
+#calculado da média
+
+nota1 = int(input('A primeira nota é:'))
+nota2 = int(input('A segunda nota é:'))
+nota3 = int(input('A terceira nota é:'))
+nota4 = int(input('A quarta nota é:'))
+
+media= (nota1 + nota2 + nota3 + nota4) / 4
+
+print(' A média é: {}'.format(media))
