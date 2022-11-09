850dde18d442569d65f40cecf2bb7957b064b1a5
gustavoferrazsilva <117416161+gustavoferrazsilva@users.noreply.github.com>
2022-11-08 21:51:19 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,8 @@
+lado1= float (input('Informe o valor do lado1:'))
+lado2= float (input('Informe o valor do lado2:'))
+#print(type(lado1))
+#print('Lado1 é númerico?' , lado1.isnumeric())
+#print('Lado1 é decimal?' , lado1.isdecimal())
+
+area = float(lado1) * float(lado2)
+print ('A area do retangulo é: {}' . format(area))
